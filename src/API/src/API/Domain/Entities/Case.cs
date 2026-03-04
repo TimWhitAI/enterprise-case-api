21465362262c19d95790dea1b1ba94f17cfda02b
Tim Whitney <whitneyhome22@gmail.com>
2026-03-04 14:54:12 -0500

    Add core domain entities

---
+++ src/API/src/API/Domain/Entities/Case.cs
@@ -0,0 +1,13 @@
+public class Case : BaseEntity
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+
+    public int OrganizationId { get; set; }
+    public int CreatedById { get; set; }
+    public int AssignedToId { get; set; }
+
+    public Organization Organization { get; set; }
+    public User CreatedBy { get; set; }
+    public User AssignedTo { get; set; }
+}
