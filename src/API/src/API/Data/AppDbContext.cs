4db78e805601f22d768531567c892e8c1fa7b02f
Tim Whitney <whitneyhome22@gmail.com>
2026-03-04 14:48:16 -0500

    Add layered architecture folders

---
+++ src/API/src/API/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) {}
+
+    public DbSet<Organization> Organizations { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<ServiceRequest> ServiceRequests { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+
+        modelBuilder.Entity<ServiceRequest>()
+            .HasOne(sr => sr.CreatedBy)
+            .WithMany()
+            .HasForeignKey(sr => sr.CreatedById)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
